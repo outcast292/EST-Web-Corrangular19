c6afeda8b5b522cea6152b098e70d97b9fa7cf90
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2020-04-12 01:09:22 +0100

    reducing api calls

--- src/app/components/compare/compare.component.ts
+++ src/app/components/compare/compare.component.ts
@@ -13,15 +13,15 @@ export class CompareComponent implements OnInit {
   country;
   countries;
   Selectedcountries = [];
-  isAdded:boolean;
-  isEmpty:boolean = true;
+  isAdded: boolean;
+  isEmpty: boolean = true;
   historicalCountry;
   cases = [];
   deaths = [];
 
   newCase;
   newDeath;
-  
+
   casesOptions = {
     responsive: true,
     title: {
@@ -30,8 +30,8 @@ export class CompareComponent implements OnInit {
       position: 'bottom',
       fontColor: '#856c8b'
     },
-    onClick : function(mouseEvent,chart){
-        console.log(chart);
+    onClick: function (mouseEvent, chart) {
+      console.log(chart);
     },
   };
 
@@ -51,14 +51,14 @@ export class CompareComponent implements OnInit {
   };
 
   constructor(private api: ApiService) {
-    this.api.getData().subscribe((data) =>{
+    this.api.getData().subscribe((data) => {
       console.log(data);
       this.countries = data;
     });
-    if(this.Selectedcountries.length = 0) {
+    if (this.Selectedcountries.length = 0) {
       this.isEmpty = true;
     }
-   }
+  }
 
   ngOnInit(): void {
   }
@@ -71,72 +71,87 @@ export class CompareComponent implements OnInit {
     }
     return color;
   }
-  
-  addCountry(){
-    if(this.Selectedcountries.includes(this.country)){
-      this.country="";
-      this.isAdded = true;
-      this.isEmpty = false;
+  //add country to SelectedCountries
+  async addCountry() {
 
+    this.Selectedcountries.forEach(element => {
+      if (element.country == this.country) {
+        this.country = "";
+        this.isAdded = true;
+        this.isEmpty = false;
+      }
       return;
-    }else{
-      this.isAdded = false;
-    }
-    if(this.country != ""){
-      this.Selectedcountries.push(this.country);
+    });
+    this.isAdded = false;
+
+    if (this.country != "") {
+      this.Selectedcountries.push(await this.getCountryData(this.country));
+
+      console.log("addCountry : " + this.Selectedcountries);
+
       this.isEmpty = false;
-      this.drawCharts();
-      this.country="";
+      this.country = "";
     }
-    
-  }
 
-  drawCharts(){
-    this.cases=[];
-    this.deaths=[];
-    this.Selectedcountries.forEach(country => {
-
-      this.api.getHistorical(country).subscribe((data) => {
-        this.historicalCountry = data;
-  
-        var color = this.RandomColor;
-        this.newCase = {
-          data: Object.values(this.historicalCountry.timeline.cases),
-          label: "Cas d'infection au " + this.historicalCountry.country,
-          borderColor: color,
-          backgroundColor: color,
-          fill: false,
-        };
-  
-        this.cases.push(this.newCase);
-  
-        this.newDeath =
-        {
-          data: Object.values(this.historicalCountry.timeline.deaths),
-          label: "Fatalités au " + this.historicalCountry.country,
-          borderColor: color,
-          backgroundColor: color,
-          fill: false,
-        };
-        this.deaths.push(this.newDeath)
-  
-        this.chartLabels = Object.keys(this.historicalCountry.timeline.cases);
-      });
-
-
-
-    })
-  }
-  removeCountry(e){
-    let index = this.Selectedcountries.indexOf(e);
-    this.countries = this.countries.filter(item => item !=e);
-    this.Selectedcountries = this.Selectedcountries.filter((item,key) =>  key!=index);
     this.drawCharts();
-}
+  }
+  //fix 
+  getCountryData(country) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.api.getHistorical(country).subscribe((data) => {
+          this.historicalCountry = data;
+           var color = this.RandomColor;
+          this.newCase = {
+            data: Object.values(this.historicalCountry.timeline.cases),
+            label: "Cas d'infection au " + this.historicalCountry.country,
+            borderColor: color,
+            backgroundColor: color,
+            fill: false,
+          };
+          this.newDeath =
+          {
+            data: Object.values(this.historicalCountry.timeline.deaths),
+            label: "Fatalités au " + this.historicalCountry.country,
+            borderColor: color,
+            backgroundColor: color,
+            fill: false,
+          };
 
-  removeAll(){
+          this.chartLabels = Object.keys(this.historicalCountry.timeline.cases);
+          var x = { "country": country, "cases": this.newCase, "deaths": this.newDeath ,"color":color };
+
+          console.log("getCountryData" + x);
+          resolve(x);
+
+        });
+      }, 100);
+    });
+  }
+
+  //charts update
+  drawCharts() {
+    console.log("drawCharts : " + this.Selectedcountries);
+
+    this.cases = [];
+    this.deaths = [];
+    this.Selectedcountries.forEach(country => {
+      this.cases.push(country.cases);
+      this.deaths.push(country.deaths);
+      this.chartLabels = Object.keys(this.historicalCountry.timeline.cases);
+    });
+  }
+  //remove from dict
+  removeCountry(e) {
+    let index = this.Selectedcountries.indexOf(e);
+    this.countries = this.countries.filter(item => item != e);
+    this.Selectedcountries = this.Selectedcountries.filter((item, key) => key != index);
+    this.drawCharts();
+  }
+
+  //remove all from dict
+  removeAll() {
     this.Selectedcountries = [];
-    this.countries = [];
     this.drawCharts();
     this.isEmpty = true;
     this.isAdded = false;
